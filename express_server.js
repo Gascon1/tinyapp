1931de6c1596785a3552e1d28ad9abd5b1770490
Mikel Gascon <mikelgascon@outlook.com>
2019-10-28 19:22:10 +0000

    added a 6 char string generator function

--- express_server.js
+++ express_server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+
+const generateRandomString = () => {
+  return Math.random(36).toString(36).slice(2, 8);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
@@ -15,8 +27,13 @@ app.get('/urls', (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.urlDatabase };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/", (req, res) => {
@@ -25,10 +42,6 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
